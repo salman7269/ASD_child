f8399e734ed150329c5868562ba89668dea220d9
salman7269 <salmanvelocity5269@gmail.com>
2024-04-29 10:10:42 +0530

    Config File

---
+++ config.py
@@ -0,0 +1,3 @@
+model_file_path=r"D:\Python_Course\Umesh_Sir\New folder\Python_coding\Python_Machine_Learning\Machine Learning Algorithms\Project_ML_autism\child_model.pkl"
+json_file_path=r"D:\Python_Course\Umesh_Sir\New folder\Python_coding\Python_Machine_Learning\Machine Learning Algorithms\Project_ML_autism\project_data.json"
+PORT_NUMBER = 5005
